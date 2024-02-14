0d8b41eb0c640db852d29cdc9c6ad2e0cb23b429
Ingryd <ingrydcduarte@gmail.com>
2024-02-14 19:44:26 -0300

    Aula de polimorfismo

--- src/polimorfismo.kt
+++ src/polimorfismo.kt
@@ -1,20 +1,28 @@
-open class dev(var salario: Float){ //interface (não herda, só implementa) e não pode criar construtores ou abstract;
-    fun bonus(){
+interface desenvolvedor{ //criação de uma interface para criação de hierarquia/herança de tais propriedades para classes filhas
+    var salario: Float
+    fun bonus ():Float
+}
+//Notas: interface (não herda, só implementa) e não pode criar construtores ou abstract;
+
+//modificador override herdando o salario e a fun bonus presente no desenvolvedor implementando um retorno diferente (aumenta 0.7)
+class backend(override var salario: Float):desenvolvedor{
+    override fun bonus():Float{
+        return salario*0.7f
     }
 }
-class mobile(salario: Float):dev(salario){ //quando nao especifica se é var, o bixo é padrão val
-
-}
-class game(salario:Float):dev(salario){
-
-}
-class back(salario: Float):dev(salario){
-
+//modificador override herdando o salario e a fun bonus presente no desenvolvedor implementando um retorno diferente (aumenta 0.6)
+class mobile(override var salario: Float):desenvolvedor{
+    override fun bonus():Float{
+        return salario*0.6f
+    }
 }
 
-//com as mudanças:
-
-
+//função para mostrar bonus chamando a interface desenvolvedor e imprimindo
+fun mostrar_bonus(d: desenvolvedor){
+    println(d.bonus())
+}
+//chama a função mostrar_bonus, que chama as classes presentes (mobile e backend) junto com o tamanho do valor (1000)
 fun main() {
-
+    mostrar_bonus(backend(1000f))
+    mostrar_bonus(mobile(1000f))
 }
