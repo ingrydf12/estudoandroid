b2ccf8964cbfb9138a01d47f8ff9ee49738eaac0
Ingryd <ingrydcduarte@gmail.com>
2024-02-15 22:34:51 -0300

    Atividade 1

---
+++ src/javaConceitos/arquivointrod.java
@@ -0,0 +1,10 @@
+package javaConceitos;
+public class arquivointrod {
+    public static void main(String[] args) {
+        int idade = 18;
+        String nome = "joaozinho";
+        System.out.println("Hello World!");
+        System.out.println(idade);
+        System.out.println(nome);
+    }
+}
