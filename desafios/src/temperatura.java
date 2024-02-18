83f59f8cfb59394b2474b4edd4696c019666bec6
Ingryd <ingrydcduarte@gmail.com>
2024-02-18 15:32:24 -0300

    Desafio de criacao de algoritmo para conversao de temperatura

--- desafios/src/temperatura.java
+++ desafios/src/temperatura.java
@@ -4,12 +4,13 @@ a converta e exiba em Kelvin (K) e Fahrenheit (F), seguindo as fórmulas: F = C 
 
 public class temperatura {
     public static void main(String[] args) {
-        double C, K, F;
+        double C, K, F; //declaração de variáveis
         C = 10; //considerando que a temp em celsius é 10
 
         F = C * 1.8 + 32;
         K = C + 273.15;
 
+        //impressão
     System.out.println("A temperatura em Celsius é: º" + C);
     System.out.println("A temperatura em Fahrenheit é: º" + F);
     System.out.println("A temperatura em Kelvin é: " + K);
